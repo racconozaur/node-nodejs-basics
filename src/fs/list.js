04203f11c63aef5b3221e0ec58a03932cdcc012c
Jasur <jasafayz01@gmail.com>
2022-12-06 01:42:21 +0500

    final

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
-const list = async () => {
+import fs from 'fs'
+
+const list = async (path) => {
     // Write your code here 
+    await fs.access(path, fs.constants.R_OK, (err) => {
+        if(!err){
+            
+            fs.readdir(path, (err, files) => {
+                let arr = []
+                files.forEach(file => {
+                    arr.push(file)
+                });
+                console.log(files);
+              });
+        }
+        else{
+            
+            console.error('FS operation failed');
+        }
+    });
 };
 
-await list();
+await list('files');
